308244e2d462e4827bd8a2ae115f4863050e04fd
Jess Bird <jessannbird@gmail.com>
2017-11-13 04:18:57 +0000

    minor adjustments

--- config/config.example.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    
-    // actual config file not committed to git
-
-    'secret': '', // secret token for jwt
-    'database': '' // uri for connection to database
-
-};
---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    // this file is not committed to git
+    
+    'secret': process.env.SECRET, // secret token for jwt
+    'database': process.env.PROD_MONGODB // uri for connection to database
+
+};
